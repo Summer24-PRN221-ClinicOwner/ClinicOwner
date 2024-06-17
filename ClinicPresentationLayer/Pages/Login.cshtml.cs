0a4b249c7cec9bcb1237342a39d7a3ef80fbf4d2
TyKay264 <khaipt4working@gmail.com>
2024-06-17 07:28:28 +0700

    feat/template-history-appointment-and-login-register

---
+++ ClinicPresentationLayer/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicPresentationLayer.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
