64a6d4e75d51bed88a7f0c741a35a0a2662b633b
Nguyen Vu <KanaeRudo@gmail.com>
2024-06-24 14:37:04 +0700

    fix slot 6 && add DurationDisplayTimeOnSlot

--- BusinessObjects/SlotDefiner.cs
+++ BusinessObjects/SlotDefiner.cs
@@ -12,6 +12,19 @@ namespace BusinessObjects
 
     public static class SlotDefiner
     {
+        public static readonly string startSlot_1Display = "7h00";
+        public static readonly string startSlot_2Display = "8h00";
+        public static readonly string startSlot_3Display = "9h00";
+        public static readonly string startSlot_4Display = "10h00";
+        public static readonly string startSlot_5Display = "11h00";
+        public static readonly string endSlot_5Display = "12h00";
+        public static readonly string startSlot_6Display = "13h00";
+        public static readonly string startSlot_7Display = "14h00";
+        public static readonly string startSlot_8Display = "15h00";
+        public static readonly string startSlot_9Display = "16h00";
+        public static readonly string startSlot_10Display = "17h00";
+        public static readonly string endSlot_10Display = "18h00";
+
         public static readonly Slot slot_1 = new() { Key = 1, DisplayTime = "Slot 1 : 7h00 - 8h00" };
         public static readonly Slot slot_2 = new() { Key = 2, DisplayTime = "Slot 2 : 8h00 - 9h00" };
         public static readonly Slot slot_3 = new() { Key = 3, DisplayTime = "Slot 3 : 9h00 - 10h00" };
@@ -24,6 +37,7 @@ namespace BusinessObjects
         public static readonly Slot slot_9 = new() { Key = 9, DisplayTime = "Slot 9 : 16h00 - 17h00" };
         public static readonly Slot slot_10 = new() { Key = 10, DisplayTime = "Slot 10 : 17h00 - 18h00" };
 
+        public static readonly List<string> sequenceDisplayTime = [startSlot_1Display, startSlot_2Display, startSlot_3Display, startSlot_4Display, startSlot_5Display, endSlot_5Display, startSlot_6Display, startSlot_7Display, startSlot_8Display, startSlot_9Display, startSlot_10Display, endSlot_10Display];
         public static readonly List<Slot> slots = [slot_1, slot_2, slot_3, slot_4, slot_5, slot_6, slot_7, slot_8, slot_9, slot_10];
         public static List<Slot> ConvertFromString(string input)
         {
@@ -32,7 +46,7 @@ namespace BusinessObjects
             var result = new List<Slot>();
             for (int i = 0; i < slots.Count; i++)
             {
-                result.Add(NewSlot(i+1));
+                result.Add(NewSlot(i + 1));
                 result.ElementAt(i).IsAvailable = input.ElementAt(i) != '0';
             }
             return result.OrderBy(item => item.Key).ToList();
@@ -61,7 +75,7 @@ namespace BusinessObjects
             return result;
         }
 
-        public static Slot NewSlot(int key) => new() { Key = key, DisplayTime = slots.ElementAt(key-1).DisplayTime, IsAvailable = true };
+        public static Slot NewSlot(int key) => new() { Key = key, DisplayTime = slots.ElementAt(key - 1).DisplayTime, IsAvailable = true };
         public static int CheckSlotRequired(string slotString, int slotRequired)
         {
 
@@ -69,7 +83,7 @@ namespace BusinessObjects
             int checkedSlot = 0;
             foreach (Slot slot in listSlot)
             {
-                if (slot.IsAvailable && slot.Key != 6)
+                if (slot.IsAvailable && (slotRequired == 1 || (slotRequired != 1 && slot.Key != 6)))
                 {
                     checkedSlot++;
                     if (checkedSlot == slotRequired)
@@ -103,6 +117,14 @@ namespace BusinessObjects
                 }
             }
             return -1;
+        }
+        public static List<Slot> DurationDiplayTimeOnSlot(List<Slot> listSlot, int requiredSlot)
+        {
+            foreach (Slot slot in listSlot)
+            {
+                slot.DisplayTime = sequenceDisplayTime.ElementAt(slot.Key - 1) + " - " + sequenceDisplayTime.ElementAt(slot.Key);
+            }
+            return listSlot;
         }
     }
 }
