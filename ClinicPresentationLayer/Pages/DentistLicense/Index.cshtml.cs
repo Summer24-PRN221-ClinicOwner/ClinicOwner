fdec29bff0b2625c738e1f402ec82a980e8b8dda
Nguyễn Vũ <115633834+KanaeRudo00@users.noreply.github.com>
2024-07-22 13:36:27 +0700

    Update dentist dentist and license

--- ClinicPresentationLayer/Pages/DentistLicense/Index.cshtml.cs
+++ ClinicPresentationLayer/Pages/DentistLicense/Index.cshtml.cs
@@ -1,9 +1,12 @@
-﻿using BusinessObjects.Entities;
+﻿using BusinessObjects;
+using BusinessObjects.Entities;
+using ClinicPresentationLayer.Authorization;
 using ClinicServices.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ClinicPresentationLayer.Pages.DentistLicense
 {
+    [CustomAuthorize(UserRoles.Dentist)]
     public class IndexModel : PageModel
     {
         private readonly IDentistService _dentistService;
@@ -14,9 +17,11 @@ namespace ClinicPresentationLayer.Pages.DentistLicense
         }
 
         public IList<License> License { get; set; } = default!;
+        public int DentistId { get; set; } = default!;
 
         public async Task OnGetAsync(int id)
         {
+            DentistId = id;
             License = _dentistService.GetDentistById(id).Licenses.ToList();
         }
     }
